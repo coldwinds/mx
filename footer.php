f03c3e519b7808b4db82da3ad64cc882a0b38a9f
kmvan <kmvan.com@gmail.com>
2015-09-29 07:06:07 +0800

    optimize footer links menu

--- footer.php
+++ footer.php
@@ -10,26 +10,29 @@ if(class_exists('theme_adbox') && !empty(theme_adbox::display_frontend('above-fo
 ?>
 <footer id="footer">
 	<div class="container">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h2 class="panel-title"><i class="fa fa-link"></i> <?= ___('Links');?></h2>
+		<!-- links -->
+		<?php if(!wp_is_mobile() && theme_cache::is_home()){ ?>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h2 class="panel-title"><i class="fa fa-link"></i> <?= ___('Links');?></h2>
+				</div>
+				<div class="panel-body">
+					<?php
+					/**
+					 * links
+					 */
+					theme_cache::wp_nav_menu([
+						'theme_location'    => 'links-footer',
+						'container'         => 'nav',
+						'menu_class'        => 'menu',
+						'menu_id' 			=> 'links-footer',
+						'fallback_cb'       => 'custom_navwalker::fallback',
+						'walker'            => new custom_navwalker
+					]);
+					?>
+				</div>
 			</div>
-			<div class="panel-body">
-				<?php
-				/**
-				 * links
-				 */
-				theme_cache::wp_nav_menu([
-					'theme_location'    => 'links-footer',
-					'container'         => 'nav',
-					'menu_class'        => 'menu',
-					'menu_id' 			=> 'links-footer',
-					'fallback_cb'       => 'custom_navwalker::fallback',
-					'walker'            => new custom_navwalker
-				]);
-				?>
-			</div>
-		</div>
+		<?php } ?>
 		<?php if(!wp_is_mobile()){ ?>
 			<div class="widget-area row hiddex-xs">
 				<?php if(!theme_cache::dynamic_sidebar('widget-area-footer')){ ?>
